6f3c7d2fea5902bec5f34ddb96ffa01cd7d6d2bd
Agnija <ilzena.agnija@gmail.com>
2023-03-12 14:37:03 +0100

    getallUsers access Admin only

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,7 +1,26 @@
+import { GETALL_USERS } from "../../gql/queries";
+import { useQuery } from "@apollo/client";
+
+interface User {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+}
+
 const Profile = () => {
+  const { data, loading, error } = useQuery(GETALL_USERS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error...</p>;
+  console.log(data);
+
   return (
     <div>
-      <p>Profile page component</p>
+      <p>My profile</p>
+      {data.getAllUsers.map((user: User) => (
+        <p key={user.id}>{user.email}</p>
+      ))}
     </div>
   );
 };
